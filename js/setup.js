4ec563b016db138c4681a84e35744b809c1062c4
AprilBoy <v.s.13.07@mail.ru>
2019-09-18 19:27:46 +0300

    WIP

--- js/setup.js
+++ js/setup.js
@@ -35,47 +35,50 @@ var EYESCOLOR = [
   'yellow',
   'green'
 ];
+var MAX_VALUE = 4;
+
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+  .content
+  .querySelector('.setup-similar-item');
 
+var getRandomArrayElem = function (arr) {
+  var min = 0;
+  var max = Math.floor(arr.length - 1);
+  return arr[Math.floor(Math.random() * (max - min + 1)) + min];
+};
 
-var wizards = [
-  {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
-  },
-  {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
-  },
-  {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
-  },
-  {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
+var getRandomWizards = function () {
+  for (i = 0; i < MAX_VALUE; i++) {
+    var wizards = [];
+    wizards.push({
+      name: getRandomArrayElem(NAMES) + ' ' + getRandomArrayElem(SURNAMES),
+      coatColor: getRandomArrayElem(COATCOLOR),
+      eyeColor: getRandomArrayElem(EYESCOLOR)
+    });
   }
-];
+  return wizards;
+};
 
+console.log(getRandomWizards());
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
 
   return wizardElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
+for (var i = 0; i < getRandomWizards().length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
